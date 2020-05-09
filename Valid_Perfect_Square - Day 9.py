499002d2903dfbe88b6ca9bc151d2e13303da2c3
Bhupendra_gehlot <57309408+Dgeneration11@users.noreply.github.com>
2020-05-09 23:10:51 +0530

    Add files via upload

---
+++ Valid_Perfect_Square - Day 9.py
@@ -0,0 +1,9 @@
+def isPerfectSquare(self, num: int) -> bool:
+        x = num
+        while x*x > num:
+            x = (x+num//x)//2
+        return x*x==num    def isPerfectSquare(self, num: int) -> bool:
+        x = num
+        while x*x > num:
+            x = (x+num//x)//2
+        return x*x==num    
